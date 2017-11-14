f726402da80f34d593b188d831340b6224d31b8c
Todd Croak-Falen <toddcf@gmail.com>
2017-11-14 14:31:27 -0800

    Added reset button functionality.

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var p1Button = document.querySelector( "#p1" );
 var p2Button = document.querySelector( "#p2" );
+var resetButton = document.querySelector( "#reset" );
 
 var p1Score = 0;
 var p2Score = 0;
 
 var p1Display = document.querySelector( "#p1Display" );
 var p2Display = document.querySelector( "#p2Display" );
+
+var gameOver = false;
 
 p1Button.addEventListener( "click", function() {
 	p1Score++;
@@ -15,4 +18,12 @@ p1Button.addEventListener( "click", function() {
 p2Button.addEventListener( "click", function() {
 	p2Score++;
 	p2Display.textContent = p2Score;
+});
+
+resetButton.addEventListener( "click", function() {
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = p1Score;
+	p2Display.textContent = p2Score;
+	gameOver = true;
 });
