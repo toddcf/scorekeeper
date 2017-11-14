9f816bc8ca79418a138d23b897588834d8b31421
Todd Croak-Falen <toddcf@gmail.com>
2017-11-14 15:26:06 -0800

    Added input functionality and improved reset capabilities.

--- app.js
+++ app.js
@@ -10,10 +10,26 @@ var p2Display = document.querySelector( "#p2Display" );
 
 var gameOver = false;
 var winningScore = 5;
-
+var winningScoreInput = document.querySelector( "input[ type = 'number' ]" ); /*Only accept number inputs.*/
 var winningScoreDisplay = document.querySelector( "#winningScoreDisplay" );
 
 winningScoreDisplay.textContent = winningScore;
+
+winningScoreInput.addEventListener( "change", function() {
+	winningScore = Number( winningScoreInput.value );
+	winningScoreDisplay.textContent = winningScore;
+	reset();
+});
+
+function reset() {
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = p1Score;
+	p2Display.textContent = p2Score;
+	p1Display.classList.remove( "green" );
+	p2Display.classList.remove( "green" );
+	gameOver = false;
+}
 
 p1Button.addEventListener( "click", function() {
 	if ( !gameOver ) {
@@ -38,11 +54,5 @@ p2Button.addEventListener( "click", function() {
 });
 
 resetButton.addEventListener( "click", function() {
-	p1Score = 0;
-	p2Score = 0;
-	p1Display.textContent = p1Score;
-	p2Display.textContent = p2Score;
-	p1Display.classList.remove( "green" );
-	p2Display.classList.remove( "green" );
-	gameOver = false;
+	reset();
 });
