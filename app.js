76b5907a06f980f4669d78ae4b9407bdf953445e
Todd Croak-Falen <toddcf@gmail.com>
2017-11-14 14:28:25 -0800

    Added p2 functionality.

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ var p1Score = 0;
 var p2Score = 0;
 
 var p1Display = document.querySelector( "#p1Display" );
+var p2Display = document.querySelector( "#p2Display" );
 
 p1Button.addEventListener( "click", function() {
 	p1Score++;
 	p1Display.textContent = p1Score;
+});
+
+p2Button.addEventListener( "click", function() {
+	p2Score++;
+	p2Display.textContent = p2Score;
 });
