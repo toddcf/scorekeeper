21cba9224b1bfdcf37820ef3d02a331f166ff130
Todd Croak-Falen <toddcf@gmail.com>
2017-11-14 14:42:33 -0800

    Set score limit for p1 and p2.

--- app.js
+++ app.js
@@ -16,13 +16,23 @@ var winningScoreDisplay = document.querySelector( "#winningScoreDisplay" );
 winningScoreDisplay.textContent = winningScore;
 
 p1Button.addEventListener( "click", function() {
-	p1Score++;
-	p1Display.textContent = p1Score;
+	if ( !gameOver ) {
+		p1Score++;
+		p1Display.textContent = p1Score;
+		if ( p1Score === winningScore ) {
+			gameOver = true;
+		}
+	}
 });
 
 p2Button.addEventListener( "click", function() {
-	p2Score++;
-	p2Display.textContent = p2Score;
+	if ( !gameOver ) {
+		p2Score++;
+		p2Display.textContent = p2Score;
+		if ( p2Score === winningScore ) {
+			gameOver = true;
+		}
+	}
 });
 
 resetButton.addEventListener( "click", function() {
